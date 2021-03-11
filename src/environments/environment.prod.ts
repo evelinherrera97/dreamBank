9b5daa52d1c28568bc6034bea4d264913fd7e82d
evelin.herrera <evelin.herrera@banistmo.com>
2021-03-11 16:15:05 -0500

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDeuyiitffbt6QHzWPSSAtSlwDcJVWS818",
+    authDomain: "dreambank-5c063.firebaseapp.com",
+    projectId: "dreambank-5c063",
+    storageBucket: "dreambank-5c063.appspot.com",
+    messagingSenderId: "501799099171",
+    appId: "1:501799099171:web:a1c84445d4145a8ef58a96",
+    measurementId: "G-K2JCJ1N384"
+  }
 };
